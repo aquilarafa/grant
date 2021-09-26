4d3fce9207e1a87f49efa9af7662cd9de2c80255
Rafael Áquila <rafaelaquilaah@hotmail.com>
2021-09-26 09:41:28 -0300

    fix: remove initial permissions parameter

--- lib/src/widgets/grant_access.dart
+++ lib/src/widgets/grant_access.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grant/grant.dart';
 import 'package:provider/provider.dart';
 
 import '../stores/internal_cubit.dart';
@@ -8,11 +7,9 @@ import '../stores/internal_store.dart';
 class GrantAccess extends StatefulWidget {
   const GrantAccess({
     required this.child,
-    this.initialPermissions = const [],
     Key? key,
   }) : super(key: key);
   final Widget child;
-  final List<PermissionBase> initialPermissions;
 
   static InternalStore storeOf(BuildContext context) {
     return Provider.of<InternalStore>(context, listen: false);
